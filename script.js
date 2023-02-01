424d182d4f77bb22ea9a63475340ca414a4c712c
Pedro <paf.victor@gmail.com>
2023-02-01 09:50:17 -0300

    Adding events and handling text

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-const POSSIBLE_CHOICES = [];
-POSSIBLE_CHOICES.push("Rock", "Paper", "Scissors");
 
-//Retorna um valor aleatório entre min_value e max_value
+
+const buttons = document.querySelectorAll("button");
+const resultInfo = document.querySelector("#result-info");
+const winnerInfo = document.querySelector("#winner");
+const drawInfo = document.querySelector("#draw");
+
+let escolhaPessoa = "";
+let playerWinCount = 0;
+let computerWinCount = 0;
+
+const POSSIBLE_CHOICES = ['rock', 'papper', 'scissor'];
+
+let finishGame = false;
+
+let updateInfo = () =>{
+    resultInfo.textContent = `Jogador: ${playerWinCount} | Computador: ${computerWinCount}`;
+}
+
+updateInfo();
+
 let getRandomNumber = (min_Value, max_Value) => {
     min_Value = Math.ceil(min_Value);
     max_Value+=1;
@@ -9,70 +26,51 @@ let getRandomNumber = (min_Value, max_Value) => {
     return Math.floor(Math.random() * (max_Value-min_Value)+min_Value);
 }
 
-let computer_choice = () => POSSIBLE_CHOICES[getRandomNumber(0,2)];
+let computerChoice = () => {return POSSIBLE_CHOICES[getRandomNumber(0,2)]}
 
-let capitalize = (string_value) => {
-    let primeira_maiuscula = string_value[0].toUpperCase();
-    string_value = string_value.toLowerCase();
-    retorno_String = string_value.replace(string_value[0], primeira_maiuscula);
-    return retorno_String;
+buttons.forEach((botao) =>{
+    botao.addEventListener('click', (e) =>{
+        let pcChoice = computerChoice();
+        let userChoice = botao.id;
+        if (!finishGame) runGame(userChoice, pcChoice);
+    })
+})
+
+
+
+let runGame = (escolhaUsuario, escolhaPC) =>{
+    if (escolhaUsuario ==="rock" && escolhaPC==="scissor" || 
+        escolhaUsuario ==="papper" && escolhaPC==="rock" || 
+        escolhaUsuario==="scissor" && escolhaPC ==="papper"){
+            playerWin();
+        }else if (escolhaPC === escolhaUsuario){
+            draw()
+        }else{
+            pcWin();
+        }
 }
-let player_choice = () => {
-    let isValidChoice = false;
-    let choice = "";
-    while(isValidChoice == false){
-        choice = prompt("Qual a sua escolha?");
-        choice = capitalize(choice);
-        POSSIBLE_CHOICES.forEach((possible_choice) => {
-            if (possible_choice === choice){
-                isValidChoice = true;
-            }
-        })   
-        if (isValidChoice == false) console.log("Entrada Inválida");    
-    }
-    return choice;
+
+let endGame = (winner = "default") =>{
+    winnerInfo.textContent=`The winner is: ${winner}`;
+    finishGame = true;
 }
 
-let player_Win_count = 0;
-let comp_Win_count = 0;
-let one_Round_Play = (round,player, computer) => {
-    let player_Win = false;
-    let computer_Win = false;
+let draw = () => { drawInfo.textContent="Empate!"}
 
-    if (player === computer){
-        alert(`Round: ${round} | Result: `+"Empate");
-    }else if (
-        player === "Rock" && computer === "Scissors" ||
-        player === "Paper" && computer === "Rock" ||
-        player === "Scissors" && computer === "Paper"
-    ){
-        player_Win = true;
-    } else{
-        computer_Win = true;
-    }
-
-    if (player_Win){
-        player_Win_count+=1;
-        alert(`Round: ${round} | Result: `+"Jogador Venceu!");
-    }else if (computer_Win){
-        comp_Win_count+=1;
-        alert(`Round: ${round} | Result: `+"Computador Venceu!");
+let playerWin = ()=> {
+    playerWinCount+=1;
+    drawInfo.textContent="";
+    updateInfo();
+    if (playerWinCount == 5){
+        endGame("Player");
     }
 }
 
-function game(){
-
-    for(let i =0; i<5; i++){
-        one_Round_Play(i+1,player_choice(), computer_choice());
-    }   
-    
-    if (player_Win_count > comp_Win_count){
-        alert(`Score computador: ${comp_Win_count}\n` + `Score player: ${player_Win_count}\n`
-        + `Player Venceu!`);
-    }else{
-        alert(`Score computador: ${comp_Win_count}\n` + `Score player: ${player_Win_count}\n`
-        + `Computador Venceu!`);
+let pcWin = () => {
+    computerWinCount+=1;
+    updateInfo();
+    drawInfo.textContent="";
+    if (computerWinCount == 5){
+        endGame("Computer");
     }
 }
-
-game();
