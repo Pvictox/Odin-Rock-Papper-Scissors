1131b80b9a6a48d2108959b87dc7c437f0afd47e
Pedro <paf.victor@gmail.com>
2023-02-02 18:49:27 -0300

    Highliting player choose | Script changes

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const resultInfo = document.querySelector("#result-info");
 const winnerInfo = document.querySelector("#winner");
 const drawInfo = document.querySelector("#draw");
 
+const choosePaper = document.querySelector("#choose-paper");
+const chooseRock = document.querySelector("#choose-rock");
+const chooseScissor = document.querySelector("#choose-scissor");
+
 let escolhaPessoa = "";
 let playerWinCount = 0;
 let computerWinCount = 0;
@@ -12,6 +16,7 @@ let computerWinCount = 0;
 const POSSIBLE_CHOICES = ['rock', 'paper', 'scissor'];
 
 let finishGame = false;
+let choosedInfoToggle = false;
 
 let updateInfo = () =>{
     resultInfo.textContent = `Jogador: ${playerWinCount} | Computador: ${computerWinCount}`;
@@ -30,13 +35,31 @@ let computerChoice = () => {return POSSIBLE_CHOICES[getRandomNumber(0,2)]}
 
 buttons.forEach((botao) =>{
     botao.addEventListener('click', (e) =>{
+        if (choosedInfoToggle){
+            clearChoose();
+        }
         let pcChoice = computerChoice();
         let userChoice = botao.id;
-        if (!finishGame) runGame(userChoice, pcChoice);
+        if (!finishGame){
+            updateChooseItem(userChoice, "Player");
+            updateChooseItem(pcChoice, "PC")
+            runGame(userChoice, pcChoice);
+
+        }
     })
 })
 
 
+let updateChooseItem = (choice, username) =>{
+    if (choice ==="rock"){
+        chooseRock.textContent+="|"+username+"|";
+    }else if (choice==="paper"){
+        choosePaper.textContent+="|"+username+"|";
+    }else{
+        chooseScissor.textContent+="|"+username+"|";
+    }
+    choosedInfoToggle=true;
+}
 
 let runGame = (escolhaUsuario, escolhaPC) =>{
     if (escolhaUsuario ==="rock" && escolhaPC==="scissor" || 
@@ -50,6 +73,12 @@ let runGame = (escolhaUsuario, escolhaPC) =>{
         }
 }
 
+let clearChoose = () => {
+    choosePaper.textContent="";
+    chooseRock.textContent="";
+    chooseScissor.textContent="";
+}
+
 let endGame = (winner = "default") =>{
     winnerInfo.textContent=`The winner is: ${winner}`;
     finishGame = true;
@@ -61,6 +90,7 @@ let playerWin = ()=> {
     playerWinCount+=1;
     drawInfo.textContent="";
     updateInfo();
+    /*clearChoose();*/
     if (playerWinCount == 5){
         endGame("Player");
     }
@@ -69,6 +99,7 @@ let playerWin = ()=> {
 let pcWin = () => {
     computerWinCount+=1;
     updateInfo();
+    /*clearChoose();*/
     drawInfo.textContent="";
     if (computerWinCount == 5){
         endGame("Computer");
